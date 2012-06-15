6e9694ed29c09ac3db89e32fa20f5d7159b5dbd8
Steve Losh <steve@stevelosh.com>
2012-06-15 11:42:54 -0400

    More changes.

--- README.markdown
+++ README.markdown
@@ -1,2 +1,10 @@
-amara-dev-blog
+Amara Dev Blog
 ==============
+
+1. `git clone` the repo.
+2. Make a virtualenv for it.
+3. `pip install -r requirements.txt`
+4. `make html`
+5. `cd output`
+6. `python -m SimpleHTTPServer 9797`
+7. Open <http://localhost:9797/> in a browser.
--- pelican.conf.py
+++ pelican.conf.py
@@ -10,21 +10,19 @@ TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG='en'
 
-# Blogroll
-LINKS =  (
-        ('Amara / Universal Subtitles', 'https://universalsubtitles.org/'),
-        ('Participatory Culture Foundation', 'http://pculture.org/'),
-)
-
-# Social widget
-SOCIAL = (
-        ('twitter', 'http://twitter.com/amarasubs'),
-        ('github', 'http://github.com/pculture/'),
-)
-
 DEFAULT_PAGINATION = False
 DISPLAY_PAGES_ON_MENU = True
 MD_EXTENSIONS = ['codehilite', 'extra', 'toc']
 
 ARTICLE_PERMALINK_STRUCTURE = '%Y-%m-%d'
 TWITTER_USERNAME = 'amarasubs'
+
+LINKS =  (
+    ('Amara / Universal Subtitles', 'https://universalsubtitles.org/'),
+    ('Participatory Culture Foundation', 'http://pculture.org/'),
+)
+
+SOCIAL = (
+    ('twitter', 'http://twitter.com/amarasubs'),
+    ('github', 'http://github.com/pculture/'),
+)
