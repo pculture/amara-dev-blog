2c4f6c0d087d54132f1e9d0e5d02b22abef8922c
Steve Losh <steve@stevelosh.com>
2012-06-15 10:48:26 -0400

    Initial skeleton.

---
+++ content/tech-notes/sample-entry.md
@@ -0,0 +1,21 @@
+Slug: some-sample-entry
+Date: 2010-12-03
+Title: My super title
+Author: Steve Losh
+Tags: python, mysql
+Category: tech notes
+
+This is a test.
+
+Hello, world!
+
+
+One
+===
+
+test
+
+Two
+---
+
+more.
---
+++ pelican.conf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Amara Team"
+SITENAME = u"Amara Dev Blog"
+SITEURL = '/'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG='en'
+
+# Blogroll
+LINKS =  (
+        ('Amara / Universal Subtitles', 'https://universalsubtitles.org/'),
+        ('Python.org', 'http://python.org'),
+        ('Jinja2', 'http://jinja.pocoo.org'),
+        ('You can modify those links in your config file', '#')
+)
+
+# Social widget
+SOCIAL = (
+        ('Twitter', 'http://twitter.com/amarasubs'),
+        ('GitHub', 'http://github.com/pculture/'),
+)
+
+DEFAULT_PAGINATION = False
+DISPLAY_PAGES_ON_MENU = True
